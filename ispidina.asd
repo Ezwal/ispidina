dd88da3be6577f5e2d8579448d6b623fae2eb192
Ezwal <ezwal@github.com>
2019-04-24 23:31:17 +0200

    now working building env

--- ispidina.asd
+++ ispidina.asd
@@ -2,13 +2,21 @@
   This file is a part of ispidina project.
 |#
 
-(defsystem "ispidina"
+(asdf:defsystem #:ispidina
   :version "0.1.0"
   :author "Ezwal"
   :license "IDGF"
-  :depends-on (#:dexador #:plump #:lquery #:lparallel #:str)
+  :serial t
+  :depends-on (#:dexador
+               #:plump
+               #:lquery
+               #:lparallel
+               #:str)
   :components ((:module "src"
                 :components
                 ((:file "ispidina"))))
   :description "rakin'"
-  :in-order-to ((test-op (test-op "ispidina-test"))))
+  :build-pathname "ispidina"
+  :build-operation "program-op"
+  :entry-point "ispidina:main")
+;; :in-order-to ((test-op (test-op "ispidina-test"))
--- src/ispidina.lisp
+++ src/ispidina.lisp
@@ -1,5 +1,7 @@
 (defpackage ispidina
-  (:use :cl))
+  (:use :cl)
+  (:export
+   :main))
 (in-package :ispidina)
 
 (defun dl+parse-page (url)
@@ -40,17 +42,22 @@
 
 ;; this may or not be useful depending on the state of the network
 ;; (setf lparallel:*kernel* (lparallel:make-kernel 4))
-;; (defun parallel-fetch-imgur-gallery (url)
-;;   (let* ((parsed-page (dl+parse-page url))
-;;          (title (select-imgur-title parsed-page))
-;;          (title-dir (directory-name title))
-;;          (images (select-imgur-images parsed-page)))
-;;     (ensure-directories-exist title-dir)
-;;     (lparallel:pmap 'vector
-;;                     (lambda (id) (fetch-img (format-id-to-link id)
-;;                                             (str:concat title-dir id ".jpg")))
-;;                     images)))
+(defun parallel-fetch-imgur-gallery (url)
+  (let* ((parsed-page (dl+parse-page url))
+         (title (select-imgur-title parsed-page))
+         (title-dir (directory-name title))
+         (images (select-imgur-images parsed-page)))
+    (ensure-directories-exist title-dir)
+    (lparallel:pmap 'vector
+                    (lambda (id) (fetch-img (format-id-to-link id)
+                                            (str:concat title-dir id ".jpg")))
+                    images)))
+
+;; 256 1920x1080 wp:
+;; Serie 56s
+;; // w 4 agents 10s
+;; // w 8 agents 4.5s
 
 (defun main ()
   (let ((argv sb-ext:*posix-argv*))
-    (fetch-imgur-gallery (first argv))))
+    (fetch-imgur-gallery (second argv))))

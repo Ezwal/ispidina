685ea4ab1fc351e5cfc2bae5a311123bdbef2f68
Ezwal <ezwal@github.com>
2019-04-25 00:22:05 +0200

    parallel raking depending on cli now

--- src/ispidina.lisp
+++ src/ispidina.lisp
@@ -29,35 +29,25 @@
 (defun fetch-img (url destination)
   (ignore-errors (dex:fetch url destination)))
 
-(defun fetch-imgur-gallery (url)
+;; perf comparison:
+;; for 256jpg 1920x1080 wp
+;; Serie => 56s
+;; // w 4 agents => 10s
+;; // w 8 agents => 4.5s
+
+(defun fetch-imgur-gallery (url &optional is-parallel?)
   (let* ((parsed-page (dl+parse-page url))
          (title (select-imgur-title parsed-page))
          (title-dir (directory-name title))
          (images (select-imgur-images parsed-page)))
     (ensure-directories-exist title-dir)
-    (map 'vector
+    (if is-parallel? (setf lparallel:*kernel* (lparallel:make-kernel 4)))
+    (funcall (if is-parallel? #'lparallel:pmap #'map)
+             'vector
           (lambda (id) (fetch-img (format-id-to-link id)
                                         (str:concat title-dir id ".jpg")))
             images)))
 
-;; this may or not be useful depending on the state of the network
-;; (setf lparallel:*kernel* (lparallel:make-kernel 4))
-(defun parallel-fetch-imgur-gallery (url)
-  (let* ((parsed-page (dl+parse-page url))
-         (title (select-imgur-title parsed-page))
-         (title-dir (directory-name title))
-         (images (select-imgur-images parsed-page)))
-    (ensure-directories-exist title-dir)
-    (lparallel:pmap 'vector
-                    (lambda (id) (fetch-img (format-id-to-link id)
-                                            (str:concat title-dir id ".jpg")))
-                    images)))
-
-;; 256 1920x1080 wp:
-;; Serie 56s
-;; // w 4 agents 10s
-;; // w 8 agents 4.5s
-
 (defun main ()
   (let ((argv sb-ext:*posix-argv*))
-    (fetch-imgur-gallery (second argv))))
+    (fetch-imgur-gallery (second argv) (third argv))))
